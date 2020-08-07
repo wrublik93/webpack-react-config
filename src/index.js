d87c18b036209700a0a524154fb533c30aae8720
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-08-07 15:12:35 +0300

    feat: add package.json

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+import {hot} from "react-hot-loader";
+
+class App extends React.Component {
+    render() {
+        return <div>Hello {this.props.name}</div>
+    }
+}
+
+const AppWithHot = hot(module)(App);
+
+var mountNode = document.getElementById("app");
+ReactDom.render(<AppWithHot name="Ivan" />, mountNode);
